52042cc499cd98eac2d0fd1dea755d0fa06b7386
DHANANJAY130 <dhananjayim81@gmail.com>
2020-03-02 01:46:24 +0530

    Stack Code Compiled.

--- Stacks/StackLL.cpp
+++ Stacks/StackLL.cpp
@@ -1,16 +1,15 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
-
 struct node1
 {
     int data;
-node1* next;
+    node1 *next;
 };
 class stackLL
 {
 private:
     node1 *top;
+
 public:
     stackLL()
     {
@@ -23,14 +22,11 @@ public:
         newnode->data = val;
         if (top == NULL)
         {
-            // cout<<"\ncase1\n";
             newnode->next = NULL;
             top = newnode;
-            // cout<<"\nend of case1\n";
         }
         else
         {
-            // cout<<"\ncase2\n";
             ptr = top;
             node1 *newnode = new node1;
             newnode->data = val;
@@ -39,111 +35,77 @@ public:
             {
                 ptr = ptr->next;
             }
-            top=newnode;
-            
-        }
-    }
-    void display()
-    {
-        node1 *ptr;
-        ptr = top;
-        cout<<"\nStack is:";
-        while (ptr != NULL)
-        {
-            cout << '\t' << ptr->data;
-            ptr = ptr->next;
-        }
-        cout<<"\n";
-    }
-    void pop()
-    {
-        node1 *ptr;
-        ptr=top;
-        top=ptr->next;
-        delete ptr;
-    }
-};
-struct node2
-{
-    char data;
-node1* next;
-};
-class stackLL
-{
-private:
-    node1 *top;
-public:
-    stackLL()
-    {
-        top = NULL;
-    }
-    void push(char val)
-    {
-        node1 *ptr;
-        node1 *newnode = new node1;
-        newnode->data = val;
-        if (top == NULL)
-        {
-            // cout<<"\ncase1\n";
-            newnode->next = NULL;
             top = newnode;
-            // cout<<"\nend of case1\n";
-        }
-        else
-        {
-            // cout<<"\ncase2\n";
-            ptr = top;
-            node1 *newnode = new node1;
-            newnode->data = val;
-            newnode->next = top;
-            while (ptr->next != NULL)
-            {
-                ptr = ptr->next;
-            }
-            top=newnode;
-            
         }
     }
     void display()
     {
         node1 *ptr;
         ptr = top;
-        cout<<"\nStack is:";
         while (ptr != NULL)
         {
             cout << '\t' << ptr->data;
             ptr = ptr->next;
         }
-        cout<<"\n";
+        cout << "\n";
     }
     void pop()
     {
         node1 *ptr;
-        ptr=top;
-        top=ptr->next;
+        ptr = top;
+        if (ptr == NULL)
+            return;
+        cout << "\n" << ptr->data << "\n";
+        top = ptr->next;
         delete ptr;
+    }
+    int Top()
+    {
+        return top->data;
     }
 };
 
+int main()
+{
+    stackLL s;
+    int data, choice,n;
+    cout<<"Enter size of stack: ";
+    cin>>n;
+    for(int i = 0; i<n ;i++)
+    {
+        cin>>data;
+        s.push(data);
+    }
 
-// int main()
-// {
-//     stackLL s;
-//     int data,n;
-//     // int inp, n;
-//     cout << "Enter no. of nodes: ";
-//     cin >> n;
-//     for (int i = 0; i < n; i++)
-//     {
-//         cin >> data;
-//         s.push(data);
-//     }
-//     cout<<"stackLL is: ";
-//     s.display();
-//     cout<<"\n";
-//     s.pop();
-//     cout<<"Popped stackLL is: ";
-//     s.display();
-//     cout<<"\n";
-//     return 0;
-// }
+    do
+    {
+        cout << "\n1.Push Elements into Stack";
+        cout << "\n2.Pop Element from the Stack";
+        cout << "\n3.Display All Elements of Stack";
+        cout << "\n4.Display Top Element of Stack";
+        cout<<"\nEnter -1 to exit";
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter Data to be pushed into stack: ";
+            cin >> data;
+            s.push(data);
+            break;
+        case 2:
+            s.pop();
+            break;
+        case 3:
+            cout << "\nStack is: ";
+            s.display();
+            cout << "\n";
+            break;
+        case 4:
+            cout << "\nTop Element is: " << s.Top() << "\n";
+            break;
+        }
+    } while (choice != -1);
+
+    return 0;
+}
